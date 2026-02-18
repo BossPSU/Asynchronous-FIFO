ed68069ecc9478e31aa441f2b01fe278d31091a2
liukee-559 <liukee@pdx.edu>
2026-02-18 15:54:47 -0800

    Update async_fifo_package.sv

--- async_fifo_package.sv
+++ async_fifo_package.sv
@@ -1,8 +1,9 @@
 package async_fifo_package;
 
-	parameter int DATA_WIDTH = 32;
-	parameter int DEPTH = 256;
+	parameter int DATA_WIDTH = 8;
+	parameter int DEPTH = 16;  //2^N
+	
 	parameter int ADDR_WIDTH = $clog2(DEPTH); // address bits
-	parameter int PTR_WIDTH = ADDR_WIDTH +1 //pointer bits (+1 wrap bit) //WIDTH = ADDR_WIDTH - 1
+	parameter int PTR_WIDTH = ADDR_WIDTH +1; //pointer bits (+1 wrap bit) //WIDTH = ADDR_WIDTH - 1
 	
 endpackage
