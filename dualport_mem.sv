015ab0cf98a8d3f11d329d98f0e76252102df748
liukee-559 <liukee@pdx.edu>
2026-02-18 02:40:50 -0800

    Update dualport_mem.sv

--- dualport_mem.sv
+++ dualport_mem.sv
@@ -1,30 +1,31 @@
 import async_fifo_package::*;
 
 module dualport_mem(
-	input logic w_clk, r_clk, w_en, r_en,
-	input logic [ADDR_WIDTH-1:0] w_addr,r_addr,
+	input logic w_clk,
+	input logic w_en,
+	input logic [ADDR_WIDTH-1:0] w_addr,
 	input logic [DATA_WIDTH-1:0] w_data,
+	
+	input logic r_clk,
+	input logic r_en,
+	input logic [ADDR_WIDTH-1:0] r_addr,
 	output logic [DATA_WIDTH-1:0] r_data
 	);
 
 	//memory array
-	logic [DATA_WIDTH-1:0] mem [DEPTH-1:0];
+	logic [DATA_WIDTH-1:0] mem [0:DEPTH-1];
 
 	//write port wclk
-	always_ff(@posedge w_clk) begin
+	always_ff @(posedge w_clk) begin
 		if (w_en) begin
-			if (w_addr < DEPTH)
-				mem[w_addr] <= w_data;
+			mem[w_addr] <= w_data;
 		end
 	end
 
 	//read port 
-	always_ff(@posedge r_clk) begin
+	always_ff @(posedge r_clk) begin
 		if(r_en) begin
-			if (r_addr < DEPTH)
-				r_data <= mem[r_addr];
-			else
-				r_data <= '0;
+			r_data <= mem[r_addr];
 		end
 	end
 	
